3dbfc38785dd688bb66a60a95642536208258d5e
anand_reddy <anandkumarreddy8456@gmail.com>
2024-11-29 19:44:30 +0530

    test the app

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
-
+import "./App.css";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Main from "./pages/Main";
+import Login from "./pages/Login";
+import Hello from "./pages/Hello";
+import Navbar from "./pages/Navbar";
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Login />} />
+          <Route path="/main" element={<Main />} />
+          <Route path="/hello" element={<Hello />} />
+          <Route path="/nav" element={<Navbar />} />
+        </Routes>
+      </Router>
     </div>
   );
 }
